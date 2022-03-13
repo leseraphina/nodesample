8372ac221d0a76480a7524fbbe05589080ebff0e
leseraphina <leseraphina@gmail.com>
2022-03-14 00:22:42 +0900

    start

---
+++ main.js
@@ -0,0 +1,10 @@
+// import _ from 'lodash';
+import $ from "jquery";
+console.log('welcome npm world');
+
+$(
+  function(){
+    $('body').css('background','#666');
+    $('h1').css({fontSize:'30px',color:'#fff'});
+  }
+)
